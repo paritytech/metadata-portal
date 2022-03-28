10ef69e756a76d0f05092a7d7cb5382a5b4dec40
Pavel Rybalko <pavel@parity.io>
2022-03-28 17:54:32 +0200

    fix frontend QR padding

--- src/components/AddToSigner.tsx
+++ src/components/AddToSigner.tsx
@@ -68,11 +68,11 @@ export default function AddToSigner({ path }: QrInfo) {
                 >
                   Scan this code with your signer device
                 </Dialog.Title>
-                <div className="mt-2 h-[180px]">
-                  <img src={process.env.PUBLIC_URL + path} className="ml-auto mr-auto" alt="loading..." />
+                <div className="min-h-[180px]">
+                  <img src={process.env.PUBLIC_URL + path} className="ml-auto mr-auto" alt="Qr code" />
                 </div>
 
-                <div className="mt-4">
+                <div>
                   <button
                     type="button"
                     className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"

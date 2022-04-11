75cd7b4d654754a60015d2e66dade14c8572dc6b
wirednkod <wirednkod@gmail.com>
2022-04-11 17:02:22 +0300

    Add mottled-library and refactor accordingly

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,37 +1,98 @@
-import React from "react";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { useState } from "react";
 import { getChains } from "../scheme";
-import Page from "./Page";
-// @ts-ignore
-import GitHubForkRibbon from "react-github-fork-ribbon";
+import QrCode from "./QrCode";
+import Specs from "./Specs";
+import AddToSigner from "./AddToSigner";
+import { BadgeCheckIcon, ExclamationCircleIcon } from "@heroicons/react/solid";
+import {
+  NetworkSlider,
+  Network,
+  getSubNetworkDetails,
+  NetworkDetails,
+  Card,
+} from "mottled-library";
+import "mottled-library/css/NetworkSlider.css";
+import "mottled-library/css/Card.css";
+import GitHub from "../assets/gh.png";
 
 export default function App() {
   const allChains = getChains();
-  const chainsName = Object.keys(allChains);
-  const routes = chainsName.map((name) => (
-    <Route
-      key={name}
-      path={name}
-      element={<Page currentName={name} allChains={allChains} />}
-    />
-  ));
+  const currentName = Object.keys(allChains)[0] || "polkadot";
+  const chain = allChains[currentName];
+  const metadataQr = chain.metadataQr;
+  const specsQr = chain.specsQr;
+  const svgClass = "inline mr-2 h-7";
+
+  const [currentNetwork, setCurrentNetwork] = useState<
+    NetworkDetails | undefined
+  >(getSubNetworkDetails(currentName));
+
+  document.body.style.backgroundColor = currentNetwork?.secondaryColor || "";
+
   return (
-    <HashRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Page currentName={chainsName[0]} allChains={allChains} />}
-        />
-        {routes}
-      </Routes>
-      <GitHubForkRibbon
-        href="https://github.com/paritytech/metadata-portal"
-        target="_blank"
-        color="black"
-        position="right-bottom"
+    <div className="flex flex-col">
+      <div
+        className="flex pt-8 pb-8 justify-around items-center"
+        style={{ backgroundColor: currentNetwork?.primaryColor }}
       >
-        Fork me on GitHub
-      </GitHubForkRibbon>
-    </HashRouter>
+        <div className="text-white w-1 font-bold text-2xl">
+          Metadata Update Portal
+        </div>
+        <NetworkSlider
+          defaultNetwork={currentNetwork?.type}
+          setNetwork={(network: NetworkDetails) => setCurrentNetwork(network)}
+          networks={Object.keys(allChains) as Network[]}
+        />
+        <div className="text-white font-bold">
+          <a
+            className="w-5"
+            href="https://github.com/paritytech/metadata-portal"
+            target={"blank"}
+          >
+            <div className="flex float-left">Github</div>
+            <div className="flex float-left">
+              <img className="w-6 ml-2" src={GitHub} />
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="flex flex-row flex-wrap justify-center pt-8">
+        {metadataQr && (
+          <Card st={{ minWidth: "500px;" }}>
+            <div className="flex justify-between p-8">
+              <h1
+                className="text-xl sm:text-4xl"
+                style={{ color: currentNetwork?.primaryColor }}
+              >
+                Metadata #{metadataQr.version}
+              </h1>
+              <div className="flex border-2 border-black rounded-xl p-2">
+                {metadataQr.signedBy ? (
+                  <div className="text-black font-normal">
+                    <BadgeCheckIcon className={svgClass} />
+                    Signed by {metadataQr.signedBy}
+                  </div>
+                ) : (
+                  <div className="text-red-500">
+                    <ExclamationCircleIcon className={svgClass} />
+                    Unsigned
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="flex">
+              <QrCode path={metadataQr.path} />
+              <div className="text-black overflow-auto p-5">
+                <Specs
+                  chainSpecs={{ ...chain }}
+                  color={currentNetwork?.primaryColor}
+                />
+                {specsQr && <AddToSigner {...specsQr} />}
+              </div>
+            </div>
+          </Card>
+        )}
+      </div>
+    </div>
   );
 }
--- src/components/Specs.tsx
+++ src/components/Specs.tsx
@@ -1,22 +1,70 @@
+import { ReactElement } from "react";
 import { ChainSpec } from "../scheme";
+import Copy from "../assets/copy.png";
 
-export default function Specs(chainSpec: ChainSpec) {
+interface SpecsProps {
+  chainSpecs: ChainSpec;
+  color?: string;
+}
+
+export const copyToClipboard = (text: string): void => {
+  const dummy = document.createElement("textarea");
+  document.body.appendChild(dummy);
+  dummy.value = text;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+};
+
+export default function Specs({ chainSpecs, color }: SpecsProps) {
+  const elipsisHash = (el: string) => {
+    const sliced = el.slice(0, 6) + "..." + el.slice(el.length - 4, el.length);
+    return (
+      <>
+        <div className="flex float-left">{sliced}</div>
+        <img
+          className="w-5 h-5 ml-2 cursor-pointer"
+          src={Copy}
+          onClick={() => copyToClipboard(el)}
+        />
+      </>
+    );
+  };
+
+  const { rpcEndpoint, genesisHash, unit, addressPrefix } = chainSpecs;
+
   return (
     <ul>
-      {row("RPC endpoint", chainSpec.rpcEndpoint)}
-      {row("Genesis hash", chainSpec.genesisHash)}
-      {row("Color", chainSpec.color)}
-      {row("Unit", chainSpec.unit)}
-      {row("Address prefix", chainSpec.addressPrefix)}
+      {row(
+        "RPC endpoint",
+        <div className="font-bold" style={{ color: color }}>
+          {rpcEndpoint}
+        </div>
+      )}
+      {row("Genesis hash", elipsisHash(genesisHash))}
+      {row(
+        "Color",
+        <>
+          <div
+            style={{ backgroundColor: color }}
+            className="w-6 rounded-md border-none block"
+          ></div>
+          <div className="ml-2">{color}</div>
+        </>
+      )}
+      {row("Unit", unit)}
+      {row("Address prefix", addressPrefix.toString())}
     </ul>
   );
 }
 
-function row(title: string, value: string | number) {
+function row(title: string, content: ReactElement | string, color = "#000000") {
   return (
     <li className="py-1">
-      <div className="text-sm text-neutral-300">{title}</div>
-      {value}
+      <div className="text-sm text-black">{title}</div>
+      <div className="flex" style={{ color }}>
+        {content}
+      </div>
     </li>
   );
 }

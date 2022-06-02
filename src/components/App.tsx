84a4e4e70ac5daa80cc50019e025756dd060ebcc
wirednkod <wirednkod@gmail.com>
2022-06-02 12:49:33 +0300

    fix error frontend test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -72,7 +72,7 @@ export default function App() {
   }, [isOpen]);
 
   document.body.style.backgroundColor = "#F5F5F5";
-  const { color } = allChains[currentName];
+  const { color } = allChains[currentName] || { color: "#9C9C9C" };
 
   return !chain ? (
     <></>

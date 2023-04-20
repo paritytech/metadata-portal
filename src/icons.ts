c107c1c0de88c6509db4ed6519f839483a4366df
Pavel Rybalko <pavel@parity.io>
2023-04-20 11:43:58 +0300

    fix #625: add rococo (#626)
    * fix #625: add rococo
    
    * signed metadata rococo 9390
    
    ---------
    
    Co-authored-by: EgorPopelyaev <egor@parity.io>

--- src/icons.ts
+++ src/icons.ts
@@ -1,11 +1,13 @@
 import polkadotSrc from "./assets/icons/polkadot.svg";
 import kusamaSrc from "./assets/icons/kusama.svg";
 import westendSrc from "./assets/icons/westend.svg";
+import rococoSrc from "./assets/icons/rococo.svg";
 
 const ICONS = {
   polkadot: polkadotSrc,
   kusama: kusamaSrc,
   westend: westendSrc,
+  rococo: rococoSrc,
 };
 
 export function icon(network: string) {

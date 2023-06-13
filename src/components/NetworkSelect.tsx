f1293c97bea8527bf61ac5a813be305cadc32372
Pavel Rybalko <pavel@parity.io>
2023-06-13 23:32:20 +0300

    add search bar (#653)

--- src/components/NetworkSelect.tsx
+++ src/components/NetworkSelect.tsx
@@ -2,6 +2,8 @@ import { Listbox } from "@headlessui/react";
 import { icon } from "../icons";
 import { Chains } from "../scheme";
 import { capitalizeFirstLetter, cn } from "../utils";
+import { useState } from "react";
+import { SearchBar } from "./SearchBar";
 
 export const NetworkSelect = ({
   chains,
@@ -11,30 +13,49 @@ export const NetworkSelect = ({
   chains: Chains;
   currentChain: string;
   onSelect: (v: string) => void;
-}) => (
-  <div>
-    <div className="text-black opacity-70 mb-4">Networks</div>
-    <Listbox value={currentChain} onChange={onSelect}>
-      <Listbox.Options static className="space-y-2">
-        {Object.keys(chains).map((chain) => (
-          <Listbox.Option key={chain} value={chain}>
-            {({ selected }) => (
-              <div
-                className={cn(
-                  "flex items-center space-x-2 p-2 transition-colors rounded-full hover:bg-neutral-100",
-                  selected && "bg-neutral-100",
-                  selected ? "cursor-default" : "cursor-pointer"
-                )}
-              >
-                <img src={icon(chain)} className="w-8 rounded-full" />
-                <div className="text-lg">
-                  {capitalizeFirstLetter(chains[chain].title)}
+}) => {
+  const chainList = Object.keys(chains);
+  const [searchString, setSearchString] = useState("");
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchString(event.target.value);
+  };
+
+  const filteredItems = chainList.filter((item) =>
+    item.toLowerCase().includes(searchString.toLowerCase())
+  );
+  return (
+    <div>
+      <div className="text-black opacity-70 mb-4">Networks</div>
+      {chainList.length > 10 && (
+        <SearchBar
+          searchString={searchString}
+          setSearchString={setSearchString}
+          onChange={handleSearch}
+        />
+      )}
+      <Listbox value={currentChain} onChange={onSelect}>
+        <Listbox.Options static className="space-y-2">
+          {filteredItems.map((chain) => (
+            <Listbox.Option key={chain} value={chain}>
+              {({ selected }) => (
+                <div
+                  className={cn(
+                    "flex items-center space-x-2 p-2 transition-colors rounded-full hover:bg-neutral-100",
+                    selected && "bg-neutral-100",
+                    selected ? "cursor-default" : "cursor-pointer"
+                  )}
+                >
+                  <img src={icon(chain)} className="w-8 rounded-full" />
+                  <div className="text-lg">
+                    {capitalizeFirstLetter(chains[chain].title)}
+                  </div>
                 </div>
-              </div>
-            )}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
-    </Listbox>
-  </div>
-);
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Listbox>
+    </div>
+  );
+};

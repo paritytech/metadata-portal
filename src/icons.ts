d891fbc3ae0aac33d7dccd6e73d38138d7ec840d
Cameron Fairchild <cameron@opentensor.dev>
2023-06-28 15:43:13 -0400

    add opentensor info

--- src/icons.ts
+++ src/icons.ts
@@ -1,11 +1,13 @@
 import polkadotSrc from "./assets/icons/polkadot.svg";
 import kusamaSrc from "./assets/icons/kusama.svg";
 import westendSrc from "./assets/icons/westend.svg";
+import finneySrc from "./assets/icons/finney_tao.svg";
 
 const ICONS = {
   polkadot: polkadotSrc,
   kusama: kusamaSrc,
   westend: westendSrc,
+  'node-subtensor': finneySrc,
 };
 
 export function icon(network: string) {

1bbf2afcd75dbb170a6113c06f2ecf59234b9ccf
William Freudenberger <w.freude@icloud.com>
2024-03-11 13:59:24 +0100

    fix: prettier

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,9 +2,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders ok", () => {
-  render(
-      <App />
-  );
+  render(<App />);
 });
 
 test("data file exists", async () => {

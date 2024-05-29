6d31bc4aa59f83f849c124c9c922554ae735dd71
Wil Wade <wil.wade@amplica.io>
2024-05-29 17:02:46 -0400

    Update for Frequency on Paseo (#83)
    ## Goal
    
    The goal of this PR is to update for Frequency on Paseo testnet over
    Frequency on Rococo testnet

--- src/icons.ts
+++ src/icons.ts
@@ -1,10 +1,11 @@
 import polkadotSrc from "./assets/icons/polkadot.svg";
 import frequencySrc from "./assets/icons/frequency.svg";
+import frequencyTestnetSrc from "./assets/icons/frequency-testnet.svg";
 
 const ICONS = {
   polkadot: polkadotSrc,
   frequency: frequencySrc,
-  "frequency-testnet": frequencySrc,
+  "frequency-testnet": frequencyTestnetSrc,
 };
 
 export function icon(network: string) {

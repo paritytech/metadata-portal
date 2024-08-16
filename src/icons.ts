e9d12ad4388c01d0d5fd4054a5fbeb0cf546e557
Egor_P <egor@parity.io>
2024-08-16 12:22:04 +0200

    Add missing networks (#792)
    * add coretim westen and rococo
    
    * add westend bridge hub
    
    * add people rococo and westend

--- src/icons.ts
+++ src/icons.ts
@@ -9,7 +9,7 @@ import rococoSrc from "./assets/icons/rococo.svg";
 import { nodesEncointerBlueSVG } from "./assets/icons/encointer-SVG";
 import { contractsPNG } from "./assets/icons/contracts-PNG";
 import peopleSrc from "./assets/icons/people.svg";
-import kusamaCoretimeSrc from "./assets/icons/kusama-coretime.svg";
+import coretimeSrc from "./assets/icons/coretime.svg";
 
 const ICONS = {
   polkadot: polkadotSrc,
@@ -20,17 +20,22 @@ const ICONS = {
   kusama: kusamaSrc,
   "kusama-statemine": assetHubSrc,
   "kusama-bridge-hub-kusama": bridgeHubBlackSrc,
-  "kusama-coretime-kusama": kusamaCoretimeSrc,
+  "kusama-coretime-kusama": coretimeSrc,
   "kusama-encointer-parachain": nodesEncointerBlueSVG,
   "kusama-people-kusama": peopleSrc,
   westend: westendSrc,
   "westend-westmint": assetHubSrc,
+  "westend-bridge-hub-westend": bridgeHubSrc,
   "westend-collectives-westend": collectivesSrc,
+  "westend-coretime-westend": coretimeSrc,
+  "westend-people-westend": peopleSrc,
   rococo: rococoSrc,
   "rococo-statemine": assetHubSrc,
   "rococo-bridge-hub-rococo": bridgeHubBlackSrc,
   "rococo-contracts-rococo": contractsPNG,
+  "rococo-coretime-rococo": coretimeSrc,
   "rococo-encointer-parachain": nodesEncointerBlueSVG,
+  "rococo-people-rococo": peopleSrc,
 };
 
 export function icon(network: string) {

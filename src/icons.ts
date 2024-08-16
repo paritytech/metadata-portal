80b1aa256f5169fb16f08833f8d2478ac7f69588
EgorPopelyaev <egor@parity.io>
2024-08-16 10:25:14 +0200

    add coretim westen and rococo

--- src/icons.ts
+++ src/icons.ts
@@ -9,7 +9,7 @@ import rococoSrc from "./assets/icons/rococo.svg";
 import { nodesEncointerBlueSVG } from "./assets/icons/encointer-SVG";
 import { contractsPNG } from "./assets/icons/contracts-PNG";
 import peopleSrc from "./assets/icons/people.svg";
-import kusamaCoretimeSrc from "./assets/icons/kusama-coretime.svg";
+import coretimeSrc from "./assets/icons/coretime.svg";
 
 const ICONS = {
   polkadot: polkadotSrc,
@@ -20,16 +20,18 @@ const ICONS = {
   kusama: kusamaSrc,
   "kusama-statemine": assetHubSrc,
   "kusama-bridge-hub-kusama": bridgeHubBlackSrc,
-  "kusama-coretime-kusama": kusamaCoretimeSrc,
+  "kusama-coretime-kusama": coretimeSrc,
   "kusama-encointer-parachain": nodesEncointerBlueSVG,
   "kusama-people-kusama": peopleSrc,
   westend: westendSrc,
   "westend-westmint": assetHubSrc,
   "westend-collectives-westend": collectivesSrc,
+  "westend-coretime-westend": coretimeSrc,
   rococo: rococoSrc,
   "rococo-statemine": assetHubSrc,
   "rococo-bridge-hub-rococo": bridgeHubBlackSrc,
   "rococo-contracts-rococo": contractsPNG,
+  "rococo-coretime-rococo": coretimeSrc,
   "rococo-encointer-parachain": nodesEncointerBlueSVG,
 };
 

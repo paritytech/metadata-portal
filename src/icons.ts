41ec039d986a76296a5f1631440bfa8bb6927160
Egor_P <egor@parity.io>
2024-10-16 09:10:24 +0200

    delte rococo info (#802)

--- src/icons.ts
+++ src/icons.ts
@@ -5,9 +5,7 @@ import bridgeHubBlackSrc from "./assets/icons/bridge-hub-black.svg";
 import collectivesSrc from "./assets/icons/collectives.svg";
 import kusamaSrc from "./assets/icons/kusama.svg";
 import westendSrc from "./assets/icons/westend.svg";
-import rococoSrc from "./assets/icons/rococo.svg";
 import { nodesEncointerBlueSVG } from "./assets/icons/encointer-SVG";
-import { contractsPNG } from "./assets/icons/contracts-PNG";
 import peopleSrc from "./assets/icons/people.svg";
 import coretimeSrc from "./assets/icons/coretime.svg";
 
@@ -29,13 +27,6 @@ const ICONS = {
   "westend-collectives-westend": collectivesSrc,
   "westend-coretime-westend": coretimeSrc,
   "westend-people-westend": peopleSrc,
-  rococo: rococoSrc,
-  "rococo-statemine": assetHubSrc,
-  "rococo-bridge-hub-rococo": bridgeHubBlackSrc,
-  "rococo-contracts-rococo": contractsPNG,
-  "rococo-coretime-rococo": coretimeSrc,
-  "rococo-encointer-parachain": nodesEncointerBlueSVG,
-  "rococo-people-rococo": peopleSrc,
 };
 
 export function icon(network: string) {
